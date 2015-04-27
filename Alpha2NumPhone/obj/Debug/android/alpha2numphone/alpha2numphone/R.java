9585258eea2d585781e59b282a3ede0bd57a0abe
samthedev <abdelkhalekdev@gmail.com>
2015-04-27 15:52:29 +0100

    Add Main conversion logic recap

--- Alpha2NumPhone/obj/Debug/android/alpha2numphone/alpha2numphone/R.java
+++ Alpha2NumPhone/obj/Debug/android/alpha2numphone/alpha2numphone/R.java
@@ -14,7 +14,10 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int myButton=0x7f050000;
+        public static final int CallButton=0x7f050003;
+        public static final int PhoneNumberText=0x7f050001;
+        public static final int TranslateButton=0x7f050002;
+        public static final int textView1=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
